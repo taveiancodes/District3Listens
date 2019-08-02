bcf02984e73d5c4232e64c5e37458aafb160a515
taveiancodes <53587233+taveiancodes@users.noreply.github.com>
2019-08-01 23:05:47 -0500

    Add files via upload

---
+++ google_script_form_ajax.js
@@ -0,0 +1,14 @@
+var $form = $('form#test-form'),
+    url = 'https://script.google.com/macros/s/AKfycbzwyclvzeKdqsx7OU5dC27v8wiRPnk-tYhGehKGosU_RPLAXG5u/exec'
+
+$('#submit-form').on('click', function(e) {
+  e.preventDefault();
+  var jqxhr = $.ajax({
+    url: url,
+    method: "GET",
+    dataType: "json",
+    data: $form.serializeObject()
+  }).success(
+    // do something
+  );
+})
